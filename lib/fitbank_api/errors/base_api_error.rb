92b74de7e48755f82349d9daaae1cfac5cbe3754
Vasil Pashov <vasil.pashov@elando.com>
2022-10-04 13:19:05 +0300

    Fix docs

--- lib/fitbank_api/errors/base_api_error.rb
+++ lib/fitbank_api/errors/base_api_error.rb
@@ -11,12 +11,18 @@ module FitBankApi
       # Wrap an error returned by FitBank API. All API calls to the bank respond with
       # 200 OK. The way to check if there is an error is to check the 'Success' field of
       # the body. The body has a 'Message' field describing the general reason for the failure.
-      # Usually it also has 'Validation' field which is an array of hashes. Each element has is
-      # of this form {'Key': '<name_of_parameter_in_the_api>', 'Value': [<array of errors>]}.
+      # Usually it also has 'Validation' field which is an array of hashes. Each element of
+      # Validation is of this form:
+      #
+      #   {
+      #     'Key': '<name_of_parameter_in_the_api>',
+      #     'Value': [<array of errors>]
+      #   }
+      #
       # @note When the Message is "Houve um erro no sistema. Tente novamente mais tarde" the Validation field
-      # is missing from the body of the response. There might be other case in which some of the fields are
-      # missing that's why we need to be careful.
-      # @param [Hash] The body of the response from FitBank
+      #   is missing from the body of the response. There might be other case in which some of the fields are
+      #   missing that's why we need to be careful.
+      # @param body [Hash] The body of the response from FitBank
       def initialize(body)
         super(body.fetch('Message', 'Unknown FitBank error'))
         @validation_errors = T.let(body.fetch('Validation', []), T::Array[T::Hash[String, T::Array[String]]])
