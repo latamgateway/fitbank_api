d3ba3ec9d90df44b1eac3de585bdf9a1ad62c6b2
Vasil Pashov <vasil.pashov@elando.com>
2022-10-17 18:43:11 +0300

    Fix tests

--- spec/factories/bank_info.rb
+++ spec/factories/bank_info.rb
@@ -8,7 +8,12 @@ FactoryBot.define do
     bank_account { ENV['LATAM_BANK_ACCOUNT'] }
     bank_account_digit { ENV['LATAM_BANK_ACCOUNT_DIGIT'] }
     initialize_with do
-      new(bank_code:, bank_agency:, bank_account:, bank_account_digit:)
+      new(
+        bank_code: bank_code,
+        bank_agency: bank_agency,
+        bank_account: bank_account,
+        bank_account_digit: bank_account_digit
+      )
     end
   end
 end
