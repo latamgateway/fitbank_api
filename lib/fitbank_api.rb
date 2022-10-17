34941ba1ffa9de91c2d0286871598823ff6599d7
Vasil Pashov <vasil.pashov@elando.com>
2022-10-17 19:32:45 +0300

    Add a post method wrapper and use it in some requests

--- lib/fitbank_api.rb
+++ lib/fitbank_api.rb
@@ -11,6 +11,8 @@ require 'fitbank_api/entities/credentials'
 
 require 'fitbank_api/errors/base_api_error'
 
+require 'fitbank_api/utils/http'
+
 # This module will contain all functionalities used to wrap FitBank REST API
 module FitBankApi
   extend T::Sig
