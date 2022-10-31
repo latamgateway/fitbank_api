b498ae99e22f723c0ec34ddc3ddb26c3c72af8d7
Ito <i.tsvetkov.4@gmail.com>
2022-10-31 23:22:17 +0200

    Require all files under lib/fitbank_api

--- lib/fitbank_api.rb
+++ lib/fitbank_api.rb
@@ -3,15 +3,9 @@
 
 require 'sorbet-runtime'
 
-require 'fitbank_api/pix/payout'
-require 'fitbank_api/pix/account_limit'
-
-require 'fitbank_api/entities/bank_info'
-require 'fitbank_api/entities/credentials'
-
-require 'fitbank_api/errors/base_api_error'
-
-require 'fitbank_api/utils/http'
+Dir.glob('fitbank_api/**/*.rb', base: __dir__).each do |filepath|
+  require_relative filepath
+end
 
 # This module will contain all functionalities used to wrap FitBank REST API
 module FitBankApi
--- lib/fitbank_api/version.rb
+++ lib/fitbank_api/version.rb
@@ -1,5 +1,5 @@
 # typed: strict
 module FitBankApi
-  VERSION = '0.1.3'
+  VERSION = '0.1.4'
 end
 
