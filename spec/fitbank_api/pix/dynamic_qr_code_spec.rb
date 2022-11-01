aed9c6b742570b24211c11c65cb7086f74a76b5e
Vasil Danielov Pashov <vasil.pashov1@gmail.com>
2022-11-01 11:17:55 +0200

    Manage dynamic QR Codes (#7)
    This adds functions to manage dynamic QR codes for PIX payments. It can:
    * Generate a code. The generate call returns the ID (inside FitBank) for the QR Code
    * Get QR Code by its ID (returned from the generate function). In order to visualize the QR code. We need to create it then request it by ID and use the base64 representation of the code.
    
    
    
    Co-authored-by: Vasil Pashov <vasil.pashov@elando.com>

---
+++ spec/fitbank_api/pix/dynamic_qr_code_spec.rb
@@ -0,0 +1,48 @@
+# typed: false
+# frozen_string_literal: true
+
+require 'date'
+require 'bigdecimal'
+require 'securerandom'
+
+RSpec.describe FitBankApi::Pix::DynamicQrCode do
+  describe 'Dynamic QR code' do
+
+    before(:all) do
+      VCR.use_cassette('pix/qr_code/dynamic/generate') do
+        latam_bank_info = build(:bank_info)
+        credentials = build(:credentials)
+
+        @qr_code_manager = described_class.new(
+          base_url: ENV.fetch('FITBANK_BASE_URL'),
+          receiver_bank_info: latam_bank_info,
+          receiver_pix_key: credentials.cnpj,
+          credentials: credentials,
+          receiver_zip_code: ENV.fetch('LATAM_ZIP_CODE')
+        )
+
+        @generate_response = @qr_code_manager.generate(
+          value: BigDecimal('20'),
+          expiartion_date: Date.today + 30,
+          id: SecureRandom.uuid
+        )
+      end
+    end
+
+    it 'can generate dynamic QR code' do
+      expect(@generate_response[:Success]).to eq('true')
+      expect(@generate_response).to have_key(:DocumentNumber)
+    end
+
+    it 'can get dynamic QR code' do
+      VCR.use_cassette('pix/qr_code/dynamic/find_by_id') do
+        response = @qr_code_manager.find_by_id(@generate_response[:DocumentNumber].to_s)
+
+        expect(response[:Success]).to eq('true')
+        expect(response).to have_key(:GetPixQRCodeByIdInfo)
+        expect(response[:GetPixQRCodeByIdInfo]).to have_key(:QRCodeBase64)
+        expect(response[:GetPixQRCodeByIdInfo][:Status]).not_to eq('Error')
+      end
+    end
+  end
+end
